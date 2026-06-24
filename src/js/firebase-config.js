e13092c8415c8d4535b352f01767fcd2d6596999
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-06-24 06:55:17 +0545

    chore: add Firebase web config (#11)

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,14 +1,15 @@
 // Firebase config for Nutritilious.
-// Paste your Firebase Web App config here from Firebase Console > Project settings > Your apps > Web app.
-// This config is public by design, but keep Firebase security rules and authorized domains strict.
+// This Firebase Web App config is public by design.
+// Keep Authentication authorized domains and database/security rules strict.
 
 window.NUTRITILIOUS_FIREBASE_CONFIG = {
-  apiKey: 'PASTE_FIREBASE_API_KEY',
-  authDomain: 'PASTE_PROJECT_ID.firebaseapp.com',
-  projectId: 'PASTE_PROJECT_ID',
-  storageBucket: 'PASTE_PROJECT_ID.appspot.com',
-  messagingSenderId: 'PASTE_MESSAGING_SENDER_ID',
-  appId: 'PASTE_FIREBASE_APP_ID'
+  apiKey: 'AIzaSyB5YDMTZKcJJ_lukhhLNBDHZuHOlwi2kBE',
+  authDomain: 'nutrilicious-82877.firebaseapp.com',
+  projectId: 'nutrilicious-82877',
+  storageBucket: 'nutrilicious-82877.firebasestorage.app',
+  messagingSenderId: '981834035729',
+  appId: '1:981834035729:web:4e49b224ec4fe1ac1e77b0',
+  measurementId: 'G-D137QWKEEK'
 };
 
 window.NUTRITILIOUS_FIREBASE_AUTH_REDIRECT_URL = `${window.location.origin}${window.location.pathname}`;
